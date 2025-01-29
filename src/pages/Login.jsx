17a4eccc3e00e661d5721efd8a9b4499f6bf5e82
Prabakaran S <159547256+PrabakranMoorthy@users.noreply.github.com>
2025-01-29 23:59:59 +0530

    manual re-render

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [token, setToken] = useState(localStorage.getItem("token") || ""); 
-  // const [updateTrigger, setUpdateTrigger] = useState(0);
+  const [updateTrigger, setUpdateTrigger] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
@@ -29,7 +29,7 @@ const Login = () => {
       // Store JWT token in localStorage or context for global state
       localStorage.setItem("token", response.data.token);
       setToken(jwtToken);
-      // setUpdateTrigger((prev) => prev + 1);
+      setUpdateTrigger((prev) => prev + 1);
       // Redirect user to homepage or dashboard after successful login
       if (response.data.user.role == "admin") {
         navigate("/admin/dashboard");
